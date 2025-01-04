fadea7a0f624e24d2d02dcd5117378dddeb1fe26
Rafiahmad567 <142226654+Rafiahmad567@users.noreply.github.com>
2025-01-04 16:22:02 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -101,7 +101,7 @@
           <div class="col-md-6 col-lg-4 mb-4">
             <div class="card shadow-sm text-center">
               <div class="icon-wrapper mx-auto mt-3">
-                <img src="/assets/image/TKJ.png" alt="TJKT" class="icon-img" />
+                <img src="/assets/image/tkj.png" alt="TJKT" class="icon-img" />
               </div>
               <div class="card-body">
                 <h5 class="card-title">TJKT</h5>
